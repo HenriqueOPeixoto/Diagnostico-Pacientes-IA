e650b70a74b2422c5b6d53a674657c48e85f98dd
Henrique Peixoto <henrique.peixoto1512@gmail.com>
2022-03-07 17:47:55 -0400

    Comentários de apoio

--- perceptron.py
+++ perceptron.py
@@ -16,13 +16,16 @@ theta = 0.6
 
 class Perceptron:
     def __init__(self):
+        # wi é o weight, ou peso, de determinada entrada
         self.w1 = 0.5
         self.w2 = 0.5
         self.w3 = 0.5
         self.w4 = 0.5
 
+        # taxa de aprendizado
         self.eta = 0.4
 
+        # limiar / threshold
         self.theta = 0.6
 
 def treinar(casos):
