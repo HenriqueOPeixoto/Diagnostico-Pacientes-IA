59ebc73336832c6d11445e882ff60d7b4a0d81fb
Henrique Peixoto <henrique.peixoto1512@gmail.com>
2022-03-07 17:15:34 -0400

    Implementação inicial da classe Caso

---
+++ caso.py
@@ -0,0 +1,10 @@
+class Caso:
+    def __init__(self, nome, febre, enjoo, manchas, dores, diagnostico):
+        self.nome = nome
+        self.febre = febre
+        self.enjoo = enjoo
+        self.manchas = manchas
+        self.dores = dores
+        self.diagnostico = diagnostico
+    
+    
--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import caso
+
 print('=' * 52)
 print('| Sistema Automatizado de Diagnóstico de Pacientes |')
 print('=' * 52)
+
+
+# caso teste -> paciente Teste, sem febre, sem enjoo, sem manchas, com dores e doente
+teste = caso.Caso('Teste', False, False, False, True, True)
