d01ad8783d4a83c6fa9dfc5dcf33139b8c224fb0
Henrique Peixoto <henrique.peixoto1512@gmail.com>
2022-03-09 19:37:44 -0400

    Teste do Treinamento do Perceptron

---
+++ testePerceptron.py
@@ -0,0 +1,16 @@
+import perceptron.perceptron as perceptron
+from casoClass import Caso
+
+ia = perceptron.Perceptron()
+
+file = open("treinamento.txt","r",encoding='utf-8')
+
+lista_casos: Caso = []
+
+for line in file:
+  valores = line.replace('\n','').split(" ")
+  teste = Caso(valores[0],valores[1],valores[2],valores[3],valores[4])
+  teste.set_diagnostico(valores[5])
+  lista_casos.append(teste)
+
+perceptron.treinar(ia, lista_casos)
