41ba940cc98b2a76318598f2da608c19837d709e
VictorHugoDS <victorhugod.s@hotmail.com>
2022-03-14 16:46:14 -0400

    implementa análise de novos casos

--- arvoreDecisao/main.py
+++ arvoreDecisao/main.py
@@ -1,10 +1,9 @@
-from arvoreDecisao.noHandler import implementa_arvore_decisao, print_niveis_abaixo
+from arvoreDecisao.noHandler import classifica_novo_caso, implementa_arvore_decisao, print_niveis_abaixo
 from arvoreDecisao.treeClass import Tree
 from casoClass import Caso
 
 def arvore_decisao():
     file = open("treinamento.txt","r",encoding='utf-8')
-
 
     lista_casos: Caso = []
 
@@ -15,5 +14,11 @@ def arvore_decisao():
         lista_casos.append(teste)
 
     arvore = Tree(lista_casos)
-    implementa_arvore_decisao(arvore.get_no())
-    print_niveis_abaixo(arvore.get_no(),0)
+    novoCaso = Caso('Luis', 'Não', 'Não', 'Pequenas', 'Sim')
+    resultado = classifica_novo_caso(arvore.get_no(),novoCaso)
+    print(novoCaso.nome + ' resultado: '+resultado)
+    novoCaso = Caso('Laura', 'Sim', 'Sim', 'Grandes', 'Sim')
+    resultado = classifica_novo_caso(arvore.get_no(),novoCaso)
+    print(novoCaso.nome + ' resultado: '+resultado)
+    # implementa_arvore_decisao(arvore.get_no())
+    # print_niveis_abaixo(arvore.get_no(),0)
--- arvoreDecisao/noHandler.py
+++ arvoreDecisao/noHandler.py
@@ -1,6 +1,7 @@
 from arvoreDecisao.libEntropia import melhor_decisao
 from casoClass import Caso
 from enums import enums as dic_enums
+from enums import diagnostico
 
 from arvoreDecisao.noClass import No 
 
@@ -14,6 +15,13 @@ def separaElementos (conjunto_elementos: list[Caso], chave):
             if ( elemento.find_value_from_string(chave) == valor):
                 dic[valor].append(elemento) 
     return dic
+
+def SeparaElementos_chave_valor(conjunto_elementos: list[Caso], chave,valor):
+    lista = []
+    for elemento in conjunto_elementos:
+        if(elemento.find_value_from_string(chave) == valor):
+            lista.append(elemento)
+    return lista
 
 def listaHomogenica(conjunto_elementos: list[Caso]):
     resp = conjunto_elementos[0].diagnostico
@@ -30,6 +38,26 @@ def implementa_arvore_decisao(no: No):
     for chave in elementos_separados:
         no_filho = no.adicionar_filho(elementos_separados[chave])
         implementa_arvore_decisao(no_filho)
+
+def classifica_novo_caso(no: No,novo_caso: Caso):
+    # print('\n\n\n')
+    # for elemento in no.conjunto_elementos:
+    #     elemento.print_caso()
+    # print('\n\n\n')
+    if(listaHomogenica(no.conjunto_elementos)):
+        chave_correta = None
+        for chave in diagnostico:
+            if(no.conjunto_elementos[0].diagnostico == diagnostico[chave]):
+                chave_correta = chave
+                break
+        novo_caso.set_diagnostico(chave_correta)
+        return novo_caso.get_diagnostico_string()
+    
+    [chave,valor_chave] = melhor_decisao(no.conjunto_elementos)
+    # print(chave)
+    valor_referencia = novo_caso.find_value_from_string(chave)
+    novo_no = No(SeparaElementos_chave_valor(no.conjunto_elementos,chave,valor_referencia),no)
+    return classifica_novo_caso(novo_no,novo_caso)
 
 def print_niveis_abaixo(no,nivel):
     print("Nível nó: ",nivel)
